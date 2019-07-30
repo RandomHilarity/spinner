fb68f1be1f36cb9d6a4a20e51ab6d492e5c60055
John Webster <johnrwebs@gmail.com>
2019-07-30 20:54:08 +0000

    added a few spaces for formatting

--- spinner2.js
+++ spinner2.js
@@ -3,9 +3,9 @@ process.stdout.write("hello from spinner2.js... \rheyyy\n");
 for (let i = 100; i <= 1700; i += 200) {
   let char = "";
   (i === 100 || i === 900 || i === 1700)? char = "|": "";
-  (i === 300 || i === 1100)? char = "/": "";
-  (i === 500 || i === 1300)? char = "-": "";
-  (i === 700 || i === 1500)? char = "\\": "";
+  (i === 300 || i === 1100) ? char = "/" : "";
+  (i === 500 || i === 1300) ? char = "-" : "";
+  (i === 700 || i === 1500) ? char = "\\" : "";
   setTimeout(() => {
     process.stdout.write(`\r${char}   `);
   }, i);
