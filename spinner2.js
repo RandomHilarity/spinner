aa6fe6644c587acc8f8b62e7753a4f57ac802a66
John Webster <johnrwebs@gmail.com>
2019-07-30 20:53:26 +0000

    spinner2 exercise, refactor to be smaller, paired with Scott

---
+++ spinner2.js
@@ -0,0 +1,14 @@
+process.stdout.write("hello from spinner2.js... \rheyyy\n");
+
+for (let i = 100; i <= 1700; i += 200) {
+  let char = "";
+  (i === 100 || i === 900 || i === 1700)? char = "|": "";
+  (i === 300 || i === 1100)? char = "/": "";
+  (i === 500 || i === 1300)? char = "-": "";
+  (i === 700 || i === 1500)? char = "\\": "";
+  setTimeout(() => {
+    process.stdout.write(`\r${char}   `);
+  }, i);
+}
+  
+// pair programming with @ScottRoberts0
